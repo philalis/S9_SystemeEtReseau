c36a96be071fca8ec1b387c6bad24d3c3c5b1ef8
ju57in3 <justine.hazan@ensea.fr>
2025-10-01 17:31:19 +0200

    avancement TP

--- TP3/main.c
+++ TP3/main.c
@@ -38,12 +38,111 @@ static void main_getattr(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *
     fuse_reply_attr(req, &stbuf, 1.0);
 }
 
+static void main_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
+                        off_t off, struct fuse_file_info *fi){
+    if (inodes[ino].mode != S_IFDIR) {
+        fuse_reply_err(req, ENOTDIR);
+        return;
+    }
+
+    struct tosfs_dentry *dentries = addr + inodes[ino].block_no * TOSFS_BLOCK_SIZE;
+
+    char buffer[size];
+    size_t total_size = 0;
+
+    for (int i = 0; i < TOSFS_BLOCK_SIZE / sizeof(struct tosfs_dentry); i++) {
+        struct tosfs_dentry *dentry = &dentries[i];
+
+        if (dentry->inode == 0) {
+            continue;
+        }
+
+        struct stat stbuf = {0};
+        stbuf.st_ino = dentry->inode;
+
+        size_t entry_size = fuse_add_direntry(req, buffer + total_size, size - total_size, dentry->name, &stbuf, total_size + 1);
+        if (entry_size > size - total_size) {
+            break;
+        }
+        total_size += entry_size;
+    }
+
+    fuse_reply_buf(req, buffer, total_size);
+}
+
+static void main_lookup(fuse_req_t req, fuse_ino_t parent, const char *name) {
+    struct fuse_entry_param e;
+
+    if (inodes[parent].mode != S_IFDIR) {
+        fuse_reply_err(req, ENOTDIR);
+        return;
+    }
+
+    struct tosfs_dentry *dentries = (struct tosfs_dentry *) ((char*)addr + inodes[parent].block_no * TOSFS_BLOCK_SIZE);
+
+    // Cross the dentries and find the file
+    for (int i = 0; i < TOSFS_BLOCK_SIZE / sizeof(struct tosfs_dentry); i++) {
+        if (strncmp(dentries[i].name, name, TOSFS_MAX_NAME_LENGTH) == 0) {
+            // File found, send the entry back
+            memset(&e, 0, sizeof(e));
+            e.ino = dentries[i].inode;
+            e.attr.st_ino = e.ino;
+            e.attr.st_uid = inodes[e.ino].uid;
+            e.attr.st_gid = inodes[e.ino].gid;
+            e.attr.st_mode = inodes[e.ino].mode;
+            e.attr.st_nlink = inodes[e.ino].nlink;
+            e.attr.st_size = inodes[e.ino].size;
+            e.attr.st_blksize = sb->block_size;
+            e.attr.st_blocks = inodes[e.ino].size / sb->block_size;
+
+            fuse_reply_entry(req, &e);
+            return;
+        }
+    }
+
+    fuse_reply_err(req, ENOENT);
+}
+
+static void main_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, struct fuse_file_info *fi) {
+    if (ino == 0 || ino > sb->inodes) {
+        fuse_reply_err(req, ENOENT);
+        return;
+    }
+
+    if ((inodes[ino].mode & S_IFMT) == S_IFDIR) {
+        fuse_reply_err(req, EISDIR);
+        return;
+    }
+
+    if (!(inodes[ino].perm & S_IRUSR)) {
+        fuse_reply_err(req, EACCES);
+        return;
+    }
+
+    // Compute the size to read
+    if (off >= inodes[ino].size) {
+        fuse_reply_buf(req, NULL, 0);
+        return;
+    }
+
+    if (off + size > inodes[ino].size) {
+        size = inodes[ino].size - off;
+    }
+
+    void *file_data_block = (void *)((char *)addr + TOSFS_BLOCK_SIZE * inodes[ino].block_no);
+
+    fuse_reply_buf(req, (char *)file_data_block + off, size);
+}
+
 static struct fuse_lowlevel_ops main_oper = {
-    .getattr	= main_getattr,
+    .getattr = main_getattr,
+    .readdir = main_readdir,
+    .lookup = main_lookup,
+    .read = main_read,
 };
 
 int main(int argc, char *argv[]) {
-    /*const char *filename = "/home/justine/CLionProjects/S9_SystemeEtReseau/TP3/Ressources FS-20251001/test_tosfs_files";
+    const char *filename = "/home/justine/CLionProjects/S9_SystemeEtReseau/TP3/Ressources FS-20251001/test_tosfs_files";
     int fd = open(filename, O_RDWR);
     if (fd == -1) {
         perror("open");
@@ -64,7 +163,7 @@ int main(int argc, char *argv[]) {
     }
 
     sb = addr;
-
+    /*
     printf("---- Superblock ----\n");
     printf("Magic number   : 0x%08x\n", sb->magic);
     printf("Block size     : %u\n", sb->block_size);
@@ -76,7 +175,7 @@ int main(int argc, char *argv[]) {
 
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
     struct fuse_chan *ch;
-    char *mountpoint;
+    char *mountpoint = NULL;
     int err = -1;
 
     if (fuse_parse_cmdline(&args, &mountpoint, NULL, NULL) != -1 &&
@@ -99,7 +198,7 @@ int main(int argc, char *argv[]) {
     fuse_opt_free_args(&args);
 
     munmap(addr, file_stat.st_size);
-    //close(fd);
+    close(fd);
 
     return err ? 1 : 0;
 }
